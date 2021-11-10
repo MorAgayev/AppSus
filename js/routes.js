f0e37f0890f32066a5c72854333f5d69432b142f
Mor-Meir <123agmor@gmail.com>
2021-11-10 13:10:18 +0200

    add axios

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,7 @@
 import homePage from "./pages/home-page.cmp.js";
 import emailApp from "./apps/email/pages/email-app.cmp.js";
 import keepApp from "./apps/keep/pages/keep-app.cmp.js";
+
 const routes = [
     {
         path: '/', 
